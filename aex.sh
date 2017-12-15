2161d07a2738bc4d8b89f54e4588d37dbd7dca59
The Hard Gamer V@RuN <gamelovr695@gmail.com>
2017-12-15 12:38:51 +0530

    Create aex.sh

---
+++ aex.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir aex
+cd aex
+repo init -u https://github.com/varunhardgamer/manifest -b nougat
+repo sync -j128
+git clone https://github.com/A6000-N/android_device_lenovo_a6000_x32 -b cm-14.1 device/lenovo/a6000
+git clone https://github.com/A6000-N/android_device_lenovo_msm8916-common_x32 -b cm-14.1 device/lenovo/msm8916-common
+git clone https://github.com/A6000-N/kernel_lenovo_msm8916_x32 -b Lineage-14.1 kernel/lenovo/a6000
+git clone https://github.com/A6000-N/proprietary-vendor_lenovo_x32 -b cm-14.1 vendor/lenovo
