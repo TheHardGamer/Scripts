af8daefa488b202ca488ac77c21fcb95c7473595
The Hard Gamer V@RuN <gamelovr695@gmail.com>
2017-12-24 13:39:26 +0530

    Update aex.sh

--- aex.sh
+++ aex.sh
@@ -15,6 +15,9 @@ git clone https://github.com/varunhardgamer/android_device_lenovo_a6000_x32 -b c
 git clone https://github.com/varunhardgamer/android_device_lenovo_msm8916-common_x32 -b cm-14.1 device/lenovo/msm8916-common
 git clone https://github.com/A6000-N/kernel_lenovo_msm8916_x32 -b Lineage-14.1 kernel/lenovo/a6000
 git clone https://github.com/A6000-N/proprietary-vendor_lenovo_x32 -b cm-14.1 vendor/lenovo
+rm -rf device/lenovo/a6000/sepolicy
+rm -rf device/lenovo/msm8916-common/sepolicy
+#Huihuihui
 source build/envsetup.sh
 lunch aosp_a6000-userdebug
 mka aex -j128
